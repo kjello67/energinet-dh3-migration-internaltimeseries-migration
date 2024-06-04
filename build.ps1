25c0a13bc3110daa417a5474247f78cf937836b1
Anita Sklander Fineid <anita.fineid@cgi.com>
2024-06-04 11:29:06 +0200

    Added -tags to build.ps1

--- build.ps1
+++ build.ps1
@@ -2,6 +2,6 @@ $version = "1.0.3"
 $now = Get-Date -UFormat "%d-%m-%Y_%T"
 $sha1 = (git rev-parse HEAD).Trim()
 
-go build -ldflags "-X main.sha1ver=$sha1 -X main.version=$version -X main.buildTime=$now"
+go build -tags timetzdata -ldflags "-X main.sha1ver=$sha1 -X main.version=$version -X main.buildTime=$now"
 
 Write-Host ".\timeseries-migration.exe -version : print version to stdout and exit"
